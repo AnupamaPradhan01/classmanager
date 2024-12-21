b8683f18c28e0d0b8d2c3b748d32e477b6f36a97
AnupamaPradhan01 <anupamakripradhan08@gmail.com>
2024-12-21 19:58:53 +0530

    created User model

--- classroom/models.py
+++ classroom/models.py
@@ -1,3 +1,8 @@
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
