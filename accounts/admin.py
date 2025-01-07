4dec7562164b02b40d2c7a4058fc85e009cafbf2
AnupamaPradhan01 <anupamakripradhan08@gmail.com>
2025-01-07 23:29:46 +0530

    created user authentication

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+
+# Register the model
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "role")  # Columns to display
+    search_fields = ("username", "email")  # Enable search by username or email
+    list_filter = ("role",)  # Filter by role
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
